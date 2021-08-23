81048acb48df2daca4efd54cc3070ce979c05765
amyllong <amyllong02@gmail.com>
2021-08-23 11:58:56 -0500

    cifar10

--- cifar10.py
+++ cifar10.py
@@ -1,63 +1,79 @@
+import tensorflow as tf
 import numpy as np
+import os
+import cv2
+
 from icecream import ic
-import random
-import cPickle
 
-def load_batch():
-    path = './cifar-10-batches-py/'
-    file = 'data_batch_1'
+from tensorflow.core.framework.types_pb2 import DataType
+import cifar_tfrecord
 
-    f = open(path+file, 'rb')
-    dict = cPickle.load(f)
-    images = dict['data']
-    #images = np.reshape(images, (10000, 3, 32, 32))
-    labels = dict['labels']
-    imagearray = np.array(images)   #   (10000, 3072)
-    labelarray = np.array(labels)   #   (10000,)
-    
-    return imagearray, labelarray
+record_file1 = 'cifar10Train.tfrecords'
+dataset1 = tf.data.TFRecordDataset(record_file1)
+dataType = cifar_tfrecord.getDataType()
 
-def create_datasets(imagearray, labelarray):
-    train_set_x = np.empty((200,3072))
-    train_set_y = np.empty((1,200),dtype=np.int16)
+record_file2 = 'cifar10Test.tfrecords'
+dataset2 = tf.data.TFRecordDataset(record_file2)
 
-    i = 0
-    j = 0
-    while (j < 200):                #   200 train images
-        x = random.randint(0,1)
-        if (labelarray[i] == 3):    #   Cats
-            train_set_x[j] = imagearray[i]
-            train_set_y[0,j] = 1    #   Cat is True
-            j+=1
-        elif (x % 2 == 0 and labelarray[i] != 3):   #    NOT Cats
-            train_set_x[j] = imagearray[i]
-            train_set_y[0,j] = 0    # Cat is False
-            j+=1
-        i+=1
-        
-    train_set_x = train_set_x.T     #   Reshape to (3072, 200) 
-    
-    test_set_x = np.empty((50,3072))                #   50 test images
-    test_set_y = np.empty((1,50),dtype=np.int16)
+#parse the tfrecord rile
+def parse_record(record):
+    name_to_features = {
+        'dimension': tf.io.FixedLenFeature([], tf.int64),
+        'label': tf.io.FixedLenFeature([], tf.int64),
+        'image_raw': tf.io.FixedLenFeature([], tf.string),
+    }
+    return tf.io.parse_single_example(record, name_to_features)
 
-    i = 0
-    j = 0
-    while (j < 50):
-        x = random.randint(0,1)
-        if (labelarray[9999-i] == 3):#  In Reverse Order is Cat
-            test_set_x[j] = imagearray[9999-i]
-            test_set_y[0,j] = 1
-            j+=1
-        elif (x % 2 == 0 and labelarray[i] != 3):
-            test_set_x[j] = imagearray[9999-i]
-            test_set_y[0,j] = 0
-            j+=1
-        i+=1
+#decode the tfrecord file
+def decode_record(record):
+    image = tf.io.decode_raw(
+        record['image_raw'], out_type=dataType, little_endian=True, fixed_length=None, name=None
+    )
+    label = record['label']
+    dimension = record['dimension']
+    image = tf.reshape(image, (dimension, dimension, 3))
+    image = tf.image.random_flip_left_right(image, seed=None)
+    image = tf.cast(image, tf.float32) / 255.0
+    return (image, label)
 
-    test_set_x = test_set_x.T       #   Reshape to (3072, 50)
+#combine parse and decode 
+def parse_and_decode(record):
+    parsed_record = parse_record(record)
+    decoded_record = decode_record(parsed_record)
+    return decoded_record
 
-    train_set_x = train_set_x / 255.  #   0-255 -> 0-1
-    test_set_x = test_set_x / 255.
+#create training dataset
+train_ds = dataset1.map(parse_and_decode)\
+           .shuffle(buffer_size=10000)\
+           .batch(batch_size=64)\
+           .prefetch(buffer_size=64)\
+           .repeat()
 
-    return train_set_x, train_set_y, test_set_x, test_set_y
+#create test dataset
+test_ds = dataset2.map(parse_and_decode)\
+            .batch(batch_size=64)\
+            .prefetch(buffer_size=64)\
+            .repeat()
 
+#create model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation='relu', kernel_initializer='he_uniform'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+#train the model
+model.fit(train_ds, batch_size = 64, epochs=60, steps_per_epoch=938)
+
+#evaluate the model, aka test it
+test_loss, test_acc = model.evaluate(test_ds, verbose = 2, steps=157)
+print('\nTest loss: ', test_loss)
+print('Test accuracy:', test_acc)
